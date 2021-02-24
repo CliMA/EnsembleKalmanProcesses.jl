c745f602f0bbde680aaf571a7f16e6553ddd3c55
odunbar <odunbar@caltech.edu>
2021-02-24 19:16:49 +0000

    source files

--- src/EnsembleKalmanProcesses.jl
+++ src/EnsembleKalmanProcesses.jl
@@ -1,7 +1,12 @@
 module EnsembleKalmanProcesses
 
-function hello()
-    "hello world"
-end
+using Distributions, Statistics, LinearAlgebra, DocStringExtensions
 
-end
+#auxilliaries
+include("ParameterDistribution.jl")
+include("DataStorage.jl")
+
+#updates:
+include("EnsembleKalmanProcess.jl")
+
+end # module
