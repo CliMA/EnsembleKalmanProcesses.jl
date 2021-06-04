6832e29d34dfe60f68373d8a6a2b4a659ee5e359
ilopezgp <ilopezgp@gmail.com>
2021-06-04 11:42:42 -0700

    WIP: EKImb

--- src/EnsembleKalmanProcess.jl
+++ src/EnsembleKalmanProcess.jl
@@ -405,6 +405,67 @@ function update_ensemble!(ekp::EnsembleKalmanProcess{FT, IT, Inversion},
     end
 end
 
+function update_ensemble!(ekp::MiniBatchKalmanProcess{FT, IT, Inversion},
+                          g::Array{FT,2}, obs_mean_in::Array{FT}, obs_cov_in::Array{FT, 2};
+                          cov_threshold::FT=0.01,
+                          Δt_new=nothing,
+                          deterministic_forward_map=true,) where {FT, IT}
+
+    # Update follows eqns. (4) and (5) of Schillings and Stuart (2017)
+    
+    #catch works when g non-square
+    if !(size(g)[2] == ekp.N_ens) 
+         throw(DimensionMismatch("ensemble size in EnsembleKalmanProcess and g do not match, try transposing g or check ensemble size"))
+    end
+    update_observations!(ekp, obs_mean_in, obs_cov_in)
+
+    # u: N_par × N_ens 
+    # g: N_obs × N_ens
+    u = get_u_final(ekp)
+    N_obs = size(g, 1)
+
+    cov_init = cov(u, dims=2)
+    cov_ug = cov(u,g, dims = 2, corrected=false) # [N_par × N_obs]
+    cov_gg = cov(g,g, dims = 2, corrected=false) # [N_obs × N_obs]
+
+    if !isnothing(Δt_new)
+        push!(ekp.Δt, Δt_new)
+    elseif isnothing(Δt_new) && isempty(ekp.Δt)
+        push!(ekp.Δt, FT(1))
+    else
+        push!(ekp.Δt, ekp.Δt[end])
+    end
+
+    # Scale noise using Δt
+    scaled_obs_noise_cov = get_obs_cov_final(ekp) / ekp.Δt[end]
+    noise = rand(MvNormal(zeros(N_obs), scaled_obs_noise_cov), ekp.N_ens)
+
+    # Add obs_mean (N_obs) to each column of noise (N_obs × N_ens) if
+    # G is deterministic
+    y = deterministic_forward_map ? (get_obs_final(ekp) .+ noise) : (get_obs_final(ekp) .+ zero(noise))
+
+    # N_obs × N_obs \ [N_obs × N_ens]
+    # --> tmp is [N_obs × N_ens]
+    tmp = (cov_gg + scaled_obs_noise_cov) \ (y - g)
+    u += (cov_ug * tmp) # [N_par × N_ens]
+
+    # store new parameters (and model outputs)
+    push!(ekp.u, DataContainer(u, data_are_columns=true))
+    push!(ekp.g, DataContainer(g, data_are_columns=true))
+    
+    # Store error
+    compute_error!(ekp)
+
+    # Check convergence
+    cov_new = cov(get_u_final(ekp), dims=2)
+    cov_ratio = det(cov_new) / det(cov_init)
+    if cov_ratio < cov_threshold
+        @warn string("New ensemble covariance determinant is less than ",
+                     cov_threshold, " times its previous value.",
+                     "\nConsider reducing the EK time step.")
+    end
+end
+
 function update_ensemble!(ekp::EnsembleKalmanProcess{FT, IT, Sampler{FT}}, g_in::Array{FT,2}) where {FT, IT}
 
     #catch works when g_in non-square 
@@ -809,8 +870,8 @@ function update_ensemble!(uki::MiniBatchKalmanProcess{FT, IT, Unscented}, g_in::
 
 end
 
-function update_observations!(ekp::MiniBatchKalmanProcess{FT, IT, P}, obs_mean_in::Array{FT}, obs_cov_in::Array{FT, 2}) where {FT<:AbstractFloat, IT<:Int, P<:Process}
-    
+function update_observations!(ekp::MiniBatchKalmanProcess, obs_mean_in::Array{FT}, obs_cov_in::Array{FT, 2}) where {FT<:AbstractFloat}
+
     push!(ekp.obs_mean, obs_mean_in)
     push!(ekp.obs_noise_cov, DataContainer(obs_cov_in, data_are_columns=true))
 
