84b6724558d10079f97d1ad95ce078e30ac5443f
ilopezgp <ilopezgp@gmail.com>
2021-07-06 16:35:16 -0700

    Fixes

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ api = [
 examples = [
     "Template Example"  => "examples/template_example.md",
     "Cloudy Example"    => "examples/Cloudy_example.md",
-    "ClimateMachine Example"    => "examples/ClimateMachine_example.md"
+    "HPC interfacing example: ClimateMachine"    => "examples/ClimateMachine_example.md"
 ]
 
 pages = [
