e8a4e4bd7567e4b1ea351e3c2ca27b544d33bc54
Navid Constantinou <navidcy@users.noreply.github.com>
2021-08-21 11:42:59 +1000

    simpler and better(?) plots

--- examples/LossMinimization/loss_minimization.jl
+++ examples/LossMinimization/loss_minimization.jl
@@ -24,13 +24,14 @@ noise_level =  1e-8
 Γy = noise_level * Matrix(I, n_obs, n_obs) 
 noise = MvNormal(zeros(n_obs), Γy)
 
-# Loss Function Minimum
+# Loss Function minimum, ``u_* = (-1, 1)``:
 u★ = [1, -1]
+nothing # hide
 
-# Loss Function (unique minimum)
+# and the loss function ``G₁(u) = \|u - u_*\|``, 
 G₁(u) = [sqrt((u[1] - u★[1])^2 + (u[2] - u★[2])^2)]
 
-y_obs  = G₁(u★) + 0 * rand(noise) 
+y_obs  = G₁(u★)
 
 # Define Prior
 prior_distns = [Parameterized(Normal(0., sqrt(1))),
@@ -68,23 +69,25 @@ u_init = get_u_prior(ekiobj)
 anim_unique_minimum = @animate for i in 1:N_iter
     u_i = get_u(ekiobj, i)
     
-    p = plot(u_i[1, :], u_i[2, :],
+    plot([u★[1]], [u★[2]],
+          seriestype = :scatter,
+         markershape = :star5,
+          markersize = 10,
+         markercolor = :red,
+               label = "optimum"
+        )
+
+    plot!(u_i[1, :], u_i[2, :],
              seriestype = :scatter,
                   xlims = extrema(u_init[1, :]),
-                  ylims = extrema(u_init[2, :])
-             )
-    
-    plot!([u★[1]], xaxis="u1", yaxis="u2", seriestype="vline",
-        linestyle = :dash, linecolor = :red, label = false,
-        title = "EKI iteration = " * string(i)
-        )
-        
-    plot!([u★[2]],
-          seriestype = "hline",
-           linestyle = :dash,
-           linecolor = :red,
-               label = "optimum"
-          )
+                  ylims = extrema(u_init[2, :]),
+                 xlabel = "u1",
+                 ylabel = "u2",
+            markeralpha = 0.6,
+            markercolor = :blue,
+                  label = "particles",
+                  title = "EKI iteration = " * string(i)
+         )    
 end
 
 gif(anim_unique_minimum, "unique_minimum.gif", fps = 1) # hide
@@ -96,13 +99,15 @@ rng_seed = 10 # 10 converges to one minima 100 converges to the other
 Random.seed!(rng_seed)
 nothing # hide
 
-# Loss Function Minima
+# Loss function minima: ``u_{1*} = (1, -1)`` and ``u_{2*} = (-1, -1)``:
 u₁★ = [ 1, -1]
 u₂★ = [-1, -1]
+nothing # hide
 
-# Loss Function (two minima)
+# and the loss function ``G₂(u)``:
 G₂(u) = [abs((u[1] - u₁★[1]) * (u[1] - u₂★[1]))^2 + (u[2] - u₁★[2])^2]
 G₂(u₁★)[1] == G₂(u₂★)[1]
+
 y_obs  = [0.0]
 
 # Define Prior
@@ -130,7 +135,9 @@ ekiobj = EnsembleKalmanProcessModule.EnsembleKalmanProcess(initial_ensemble,
 #
 for i in 1:N_iter
     params_i = get_u_final(ekiobj)
+    
     g_ens = hcat([G₂(params_i[:, i]) for i in 1:N_ens]...)
+    
     EnsembleKalmanProcessModule.update_ensemble!(ekiobj, g_ens)
 end
 
@@ -138,25 +145,33 @@ u_init = get_u_prior(ekiobj)
 
 anim_two_minima = @animate for i in 1:N_iter
     u_i = get_u(ekiobj, i)
-    p = plot(u_i[1, :], u_i[2, :],
+
+    plot([u₁★[1]], [u₁★[2]],
+            seriestype = :scatter,
+           markershape = :star5,
+            markersize = 10,
+           markercolor = :red,
+                 label = "optima 1")
+     
+    plot!([u₂★[1]], [u₂★[2]],
+            seriestype = :scatter,
+           markershape = :star5,
+            markersize = 10,
+           markercolor = :green,
+                 label = "optima 2")
+
+    plot!(u_i[1, :], u_i[2, :],
              seriestype = :scatter,
                   xlims = (-2, 2),
-                  ylims = (-2, 2)
-             )
-             
-    plot!([1], xaxis="u1", yaxis="u2", seriestype="vline",
-        linestyle=:dash, linecolor=:red, label = false,
-        title = "EKI iteration = " * string(i)
-        )
-        
-    plot!([-1], seriestype="hline", linestyle=:dash, linecolor=:red, label = "optima 1")
-
-    plot!([-1], xaxis="u1", yaxis="u2", seriestype="vline",
-        linestyle=:dash, linecolor=:green, label = false,
-        title = "EKI iteration = " * string(i)
-        )
-        
-    plot!([-1], seriestype="hline", linestyle=:dash, linecolor=:green, label = "optima 2")
+                  ylims = (-2, 2),
+                 xlabel = "u1",
+                 ylabel = "u2",
+            markeralpha = 0.6,
+            markercolor = :blue,
+                  label = "particles",
+                  title = "EKI iteration = " * string(i)
+         )
+      
 end
 
 gif(anim_two_minima, "two_minima.gif", fps = 1) # hide
