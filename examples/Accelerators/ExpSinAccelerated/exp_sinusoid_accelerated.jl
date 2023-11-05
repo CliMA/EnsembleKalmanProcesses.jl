10c71714677869536f1cd012738afbf9b7136ad9
sydneyvernon <sydneyv@gmail.com>
2023-11-04 18:03:50 -0700

    working exp sin and lorenz examples

--- examples/Accelerators/ExpSinAccelerated/exp_sinusoid_accelerated.jl
+++ examples/Accelerators/ExpSinAccelerated/exp_sinusoid_accelerated.jl
@@ -51,19 +51,11 @@ y = G(theta_true) .+ rand(noise_dist)
 
 prior_u1 = constrained_gaussian("amplitude", 2, 0.1, 0, 10)
 prior_u2 = constrained_gaussian("vert_shift", 0, 0.5, -10, 10)
-prior_wide = combine_distributions([prior_u1, prior_u2])
-
-prior_u1 = constrained_gaussian("amplitude", 3, 0.1, 0, 10)
-prior_u2 = constrained_gaussian("vert_shift", 2, 0.5, -10, 10)
-prior_shifted = combine_distributions([prior_u1, prior_u2])
-
-prior_u1 = constrained_gaussian("amplitude", 1, 0.1, 0, 10)
-prior_u2 = constrained_gaussian("vert_shift", 0.8, 0.5, -10, 10)
-prior_centered = combine_distributions([prior_u1, prior_u2])
+prior = combine_distributions([prior_u1, prior_u2])
 
 # To compare the two EKI methods, we will average over several trials, 
 # allowing the methods to run with different initial ensembles and noise samples.
-N_ensembles = [5, 10, 20, 50]
+N_ens = 10
 N_iterations = 15
 N_trials = 50
 
@@ -73,36 +65,24 @@ function cost(theta)
     return log.(norm(inv(Γ) .^ 0.5 * (G(theta) .- y)) .^ 2)
 end
 
-global conv_plots = []
-
 ## Solving the inverse problem
 
-for (prior, prior_name, ylim) in [
-    (prior_wide, "center [2,0]", (-2, 7)),
-    (prior_shifted, "center [3,2]", (3, 16)),
-    (prior_centered, "true center [1,0.8]", (-7.5, 4.5)),
-]
-    for N_ensemble in N_ensembles
         # Preallocate so we can track and compare convergences of the methods
-        all_convs = zeros(N_trials, N_iterations + 1)
-        all_convs_acc = zeros(N_trials, N_iterations + 1)
+        all_convs = zeros(N_trials, N_iterations)
+        all_convs_acc = zeros(N_trials, N_iterations)
 
         for trial in 1:N_trials
             # We now generate the initial ensemble and set up two EKI objects, one using an accelerator, 
             # to compare convergence.
-            initial_ensemble = EKP.construct_initial_ensemble(rng, prior, N_ensemble)
+            initial_ensemble = EKP.construct_initial_ensemble(rng, prior, N_ens)
             accelerator = NesterovAccelerator()
 
             ensemble_kalman_process = EKP.EnsembleKalmanProcess(initial_ensemble, y, Γ, Inversion(); rng = rng)
             ensemble_kalman_process_acc =
                 EKP.EnsembleKalmanProcess(initial_ensemble, y, Γ, Inversion(); accelerator = accelerator, rng = rng)
 
-            global convs = zeros(N_iterations + 1)
-            global convs_acc = zeros(N_iterations + 1)
-
-            # Record cost of initial parameters
-            convs[1] = cost(mean(get_ϕ_final(prior, ensemble_kalman_process), dims = 2))
-            convs_acc[1] = cost(mean(get_ϕ_final(prior, ensemble_kalman_process_acc), dims = 2))
+            global convs = zeros(N_iterations)
+            global convs_acc = zeros(N_iterations)
 
             # We are now ready to carry out the inversion. At each iteration, we get the
             # ensemble from the last iteration, apply ``G(\theta)`` to each ensemble member,
@@ -112,47 +92,31 @@ for (prior, prior_name, ylim) in [
                 params_i = get_ϕ_final(prior, ensemble_kalman_process)
                 params_i_acc = get_ϕ_final(prior, ensemble_kalman_process_acc)
 
-                G_ens = hcat([G(params_i[:, i]) for i in 1:N_ensemble]...)
-                G_ens_acc = hcat([G(params_i_acc[:, i]) for i in 1:N_ensemble]...)
+                G_ens = hcat([G(params_i[:, i]) for i in 1:N_ens]...)
+                G_ens_acc = hcat([G(params_i_acc[:, i]) for i in 1:N_ens]...)
 
                 EKP.update_ensemble!(ensemble_kalman_process, G_ens, deterministic_forward_map = false)
                 EKP.update_ensemble!(ensemble_kalman_process_acc, G_ens_acc, deterministic_forward_map = false)
 
-                convs[i + 1] = cost(mean(params_i, dims = 2))
-                convs_acc[i + 1] = cost(mean(params_i_acc, dims = 2))
+                convs[i] = cost(mean(params_i, dims = 2))
+                convs_acc[i] = cost(mean(params_i_acc, dims = 2))
             end
-
             all_convs[trial, :] = convs
             all_convs_acc[trial, :] = convs_acc
         end
 
-        conv_plot = plot(titlefont = font(11), guidefont = font(7))
-        gr(size = (470 * 0.8, 750 * 0.8))
-        if N_ensemble == N_ensembles[end]
-            gr(size = (470 * 0.8, 750 * 0.8), legend = true)
-            plot!(0:N_iterations, mean(all_convs, dims = 1)[:], color = :black, label = "EKI (traditional)")
-            plot!(0:N_iterations, mean(all_convs_acc, dims = 1)[:], color = :red, label = "EKI with momentum")
-            xlabel!("Iteration")
-            ylabel!("log(Cost)")
-            ylims!(ylim)
-        else
-            plot!(0:N_iterations, mean(all_convs, dims = 1)[:], color = :black, label = "")
-            plot!(0:N_iterations, mean(all_convs_acc, dims = 1)[:], color = :red, label = "")
-            ylims!(ylim)
-        end
+        gr(size = (400,400), legend = true)
+        p = plot(1:N_iterations, mean(all_convs, dims = 1)[:], color = :black, label = "No acceleration")
+        plot!(1:N_iterations, mean(all_convs_acc, dims = 1)[:], color = :red, label = "Nesterov")
+        # error bars
+        plot!(1:N_iterations, (mean(all_convs, dims=1)[:] + std(all_convs, dims=1)[:]/sqrt(N_trials)), color = :black, ls= :dash, label = "")
+        plot!(1:N_iterations, (mean(all_convs, dims=1)[:] - std(all_convs, dims=1)[:]/sqrt(N_trials)), color = :black, ls= :dash, label = "")
+        plot!(1:N_iterations, (mean(all_convs_acc, dims=1)[:] + std(all_convs_acc, dims=1)[:]/sqrt(N_trials)), color = :red, ls= :dash, label = "")
+        plot!(1:N_iterations, (mean(all_convs_acc, dims=1)[:] - std(all_convs_acc, dims=1)[:]/sqrt(N_trials)), color = :red, ls= :dash, label = "")
 
-        title!("N_ens = " * string(N_ensemble) * ", " * prior_name)
-        push!(conv_plots, conv_plot)
+        xlabel!("Iteration")
+        ylabel!("log(Cost)")
+        title!("EKI convergence on exp sin IP")
 
-    end
-end
-
-println(conv_plots)
-## compile plots
-p1 = plot(conv_plots[1], conv_plots[2], conv_plots[3], conv_plots[4], layout = (4, 1))
-p2 = plot(conv_plots[5], conv_plots[6], conv_plots[7], conv_plots[8], layout = (4, 1))
-p3 = plot(conv_plots[9], conv_plots[10], conv_plots[11], conv_plots[12], layout = (4, 1))
-
-savefig(p1, joinpath(fig_save_directory, "exp_sin_multi_comparison_a.pdf"))
-savefig(p2, joinpath(fig_save_directory, "exp_sin_multi_comparison_b.pdf"))
-savefig(p3, joinpath(fig_save_directory, "exp_sin_multi_comparison_c.pdf"))
+        display(p)
+        savefig(p, joinpath(fig_save_directory, "exp_sin.png"))
--- examples/Accelerators/LorenzAccelerated/lorenz_accelerated.jl
+++ examples/Accelerators/LorenzAccelerated/lorenz_accelerated.jl
@@ -1,267 +1,121 @@
-include("GModel.jl") # Contains Lorenz 96 source code
-
-# Import modules
-using Distributions  # probability distributions and associated functions
+using Distributions
 using LinearAlgebra
-using StatsPlots
-using Plots
 using Random
-using JLD2
 
-# CES 
+using Plots
+
 using EnsembleKalmanProcesses
-using EnsembleKalmanProcesses.Observations
-using EnsembleKalmanProcesses.DataContainers
 using EnsembleKalmanProcesses.ParameterDistributions
-
+using EnsembleKalmanProcesses.Localizers
+import EnsembleKalmanProcesses: construct_mean, construct_cov, construct_sigma_ensemble
 const EKP = EnsembleKalmanProcesses
-rng_seed = 4137
-rng = Random.seed!(Random.GLOBAL_RNG, rng_seed)
 
-# Output figure save directory
-homedir = pwd()
-println(homedir)
-figure_save_directory = homedir * "/output/"
-data_save_directory = homedir * "/output/"
-if ~isdir(figure_save_directory)
-    mkdir(figure_save_directory)
-end
-if ~isdir(data_save_directory)
-    mkdir(data_save_directory)
-end
-
-# Governing settings
-# Characteristic time scale
-τc = 5.0 # days, prescribed by the L96 problem
-# Stationary or transient dynamics
-dynamics = 2 # Transient is 2
-# Statistics integration length
-# This has to be less than 360 and 360 must be divisible by Ts_days
-Ts_days = 90.0 # Integration length in days
-# Stats type, which statistics to construct from the L96 system
-# 4 is a linear fit over a batch of length Ts_days
-# 5 is the mean over a batch of length Ts_days
-stats_type = 5
-
-
-###
-###  Define the (true) parameters
-###
-# Define the parameters that we want to learn
-F_true = 8.0 # Mean F
-A_true = 2.5 # Transient F amplitude
-ω_true = 2.0 * π / (360.0 / τc) # Frequency of the transient F
-
-if dynamics == 2
-    params_true = [F_true, A_true]
-    param_names = ["F", "A"]
-else
-    params_true = [F_true]
-    param_names = ["F"]
-end
-n_param = length(param_names)
-params_true = reshape(params_true, (n_param, 1))
-
-println(n_param)
-println(params_true)
-
-
-###
-###  Define the parameter priors
-###
-
-if dynamics == 2
-    prior_means = [F_true + 1.0, A_true + 0.5]
-    prior_stds = [2.0, 0.5 * A_true]
-    prior_F = constrained_gaussian(param_names[1], prior_means[1], prior_stds[1], 0, Inf)
-    prior_A = constrained_gaussian(param_names[2], prior_means[2], prior_stds[2], 0, Inf)
-    priors = combine_distributions([prior_F, prior_A])
-else
-    priors = constrained_gaussian("F", F_true, 1.0, 0, Inf)
-end
-
-
-###
-###  Define the data from which we want to learn the parameters
-###
-data_names = ["y0", "y1"]
-
-
-###
-###  L96 model settings
-###
-
-# Lorenz 96 model parameters
-# Behavior changes depending on the size of F, N
-N = 36
-dt = 1 / 64.0
-# Start of integration
-t_start = 800.0
-# Data collection length
-if dynamics == 1
-    T = 2.0
-else
-    T = 360.0 / τc
-end
-# Batch length
-Ts = 5.0 / τc # Nondimensionalize by L96 timescale
-# Integration length
-Tfit = Ts_days / τc
-# Initial perturbation
-Fp = rand(Normal(0.0, 0.01), N);
-kmax = 1
-# Prescribe variance or use a number of forward passes to define true interval variability
-var_prescribe = false
-# Use CES to learn ω?
-ω_fixed = true
-
-# Settings
-# Constructs an LSettings structure, see GModel.jl for the descriptions
-lorenz_settings =
-    GModel.LSettings(dynamics, stats_type, t_start, T, Ts, Tfit, Fp, N, dt, t_start + T, kmax, ω_fixed, ω_true);
-lorenz_params = GModel.LParams(F_true, ω_true, A_true)
-
-###
-###  Generate (artificial) truth samples
-###  Note: The observables y are related to the parameters θ by:
-###        y = G(θ) + η
-###
-
-# Lorenz forward
-# Input: params: [N_params, N_ens]
-# Output: gt: [N_data, N_ens]
-# Dropdims of the output since the forward model is only being run with N_ens=1 
-# corresponding to the truth construction
-gt = dropdims(GModel.run_G_ensemble(params_true, lorenz_settings), dims = 2)
-
-# Compute internal variability covariance
-if var_prescribe == true
-    n_samples = 100
-    yt = zeros(length(gt), n_samples)
-    noise_level = 0.05
-    Γy = noise_level * convert(Array, Diagonal(gt))
-    μ = zeros(length(gt))
-    # Add noise
-    for i in 1:n_samples
-        yt[:, i] = gt .+ rand(MvNormal(μ, Γy))
+function rk4(f::F, y0::Array{Float64, 1}, t0::Float64, t1::Float64, h::Float64; inplace::Bool = true) where {F}
+    y = y0
+    n = round(Int, (t1 - t0) / h)
+    t = t0
+    if ~inplace
+        hist = zeros(n, length(y0))
     end
-else
-    println("Using truth values to compute covariance")
-    n_samples = 20
-    yt = zeros(length(gt), n_samples)
-    for i in 1:n_samples
-        lorenz_settings_local = GModel.LSettings(
-            dynamics,
-            stats_type,
-            t_start + T * (i - 1),
-            T,
-            Ts,
-            Tfit,
-            Fp,
-            N,
-            dt,
-            t_start + T * (i - 1) + T,
-            kmax,
-            ω_fixed,
-            ω_true,
-        )
-        yt[:, i] = GModel.run_G_ensemble(params_true, lorenz_settings_local)
+    for i in 1:n
+        k1 = h * f(t, y)
+        k2 = h * f(t + 0.5 * h, y + 0.5 * k1)
+        k3 = h * f(t + 0.5 * h, y + 0.5 * k2)
+        k4 = h * f(t + h, y + k3)
+        y = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        if ~inplace
+            hist[i, :] = y
+        end
+        t = t0 + i * h
     end
-    # Variance of truth data
-    #Γy = convert(Array, Diagonal(dropdims(mean((yt.-mean(yt,dims=1)).^2,dims=1),dims=1)))
-    # Covariance of truth data
-    Γy = cov(yt, dims = 2)
-
-    println(Γy)
+    if ~inplace
+        return hist
+    else
+        return y
+    end
 end
 
+function lorenz96(t, u, p)
+    N = p["N"]
+    F = 8
 
-# Construct observation object
-truth = Observations.Observation(yt, Γy, data_names)
-truth_sample = truth.mean
-###
-###  Calibrate: Ensemble Kalman Inversion
-###
+    du = similar(u)
 
-# L96 settings for the forward model in the EKP
-# Here, the forward model for the EKP settings can be set distinctly from the truth runs
-lorenz_settings_G = lorenz_settings; # initialize to truth settings
+    for i in 1:N
+        du[i] = (u[mod(i + 1, 1:N)] - u[mod(i - 2, 1:N)]) * u[mod(i - 1, 1:N)] - u[i] + F
+    end
 
-# EKP parameters
-N_ens = 20 # number of ensemble members
-N_iter = 10 # number of EKI iterations
-# initial parameters: N_params x N_ens
-initial_params = construct_initial_ensemble(rng, priors, N_ens)
-
-ekiobj = EKP.EnsembleKalmanProcess(initial_params, truth_sample, truth.obs_noise_cov, Inversion())
-ekiobj_acc = EKP.EnsembleKalmanProcess(initial_params, truth_sample, truth.obs_noise_cov, Inversion(), accelerator=NesterovAccelerator())
-
-# EKI iterations
-println("EKP inversion error:")
-err = zeros(N_iter)
-err_acc = zeros(N_iter)
-for i in 1:N_iter
-    params_i = get_ϕ_final(priors, ekiobj) # the `ϕ` indicates that the `params_i` are in the constrained space    
-    g_ens = GModel.run_G_ensemble(params_i, lorenz_settings_G)
-    EKP.update_ensemble!(ekiobj, g_ens)
-    err[i] = get_error(ekiobj)[end] #mean((params_true - mean(params_i,dims=2)).^2)
-    println("Iteration: " * string(i) * ", Error: " * string(err[i]))
-
-    params_i = get_ϕ_final(priors, ekiobj_acc) # the `ϕ` indicates that the `params_i` are in the constrained space    
-    g_ens = GModel.run_G_ensemble(params_i, lorenz_settings_G)
-    EKP.update_ensemble!(ekiobj_acc, g_ens)
-    err_acc[i] = get_error(ekiobj_acc)[end]
-    println("Iteration: " * string(i) * ", Error (Nesterov): " * string(err_acc[i]))
+    return copy(du)
 end
 
-# EKI results: Has the ensemble collapsed toward the truth?
-println("True parameters: ")
-println(params_true)
+D = 20
 
-println("\nEKI results:")
-println(get_ϕ_mean_final(priors, ekiobj))
+lorenz96_sys = (t, u) -> lorenz96(t, u, Dict("N" => D))
 
-u_stored = get_u(ekiobj, return_array = false)
-g_stored = get_g(ekiobj, return_array = false)
+# Seed for pseudo-random number generator
+rng_seed = 42
+rng = Random.MersenneTwister(rng_seed)
+dt = 0.05
+y0 = rk4(lorenz96_sys, randn(D), 0.0, 1000.0, dt)
 
-@save data_save_directory * "parameter_storage.jld2" u_stored
-@save data_save_directory * "data_storage.jld2" g_stored
+# Lorenz96 initial condition problem - Section 6.3 of Tong and Morzfeld (2022)
+G(u) = mapslices((u) -> rk4(lorenz96_sys, u, 0.0, 0.4, dt), u, dims = 1)
+p = D
+# Generate random truth
+y = y0 + randn(D)
+Γ = 1.0 * I
 
-#plots in unconstrained space
-gr(size = (600, 600))
-u_init = get_u_prior(ekiobj)
-unconstrained_params_true = transform_constrained_to_unconstrained(priors, params_true)
-anim_ekp_unconst_lorenz = @animate for i in 1:N_iter
-    u_i = get_u(ekiobj, i)
-    p = plot(u_i[1, :], u_i[2, :], seriestype = :scatter, xlims = extrema(u_init[1, :]), ylims = extrema(u_init[2, :]))
-    vline!(
-        [unconstrained_params_true[1]],
-        xaxis = "u1",
-        yaxis = "u2",
-        linestyle = :dash,
-        linecolor = :red,
-        label = false,
-        title = "EKI iteration = " * string(i),
-    )
-    hline!([unconstrained_params_true[2]], linestyle = :dash, linecolor = :red, label = "optimum")
+#### Define prior information on parameters
+priors = map(1:p) do i
+    constrained_gaussian(string("u", i), 0.0, 1.0, -Inf, Inf)
 end
-gif(anim_ekp_unconst_lorenz, joinpath(figure_save_directory, "ekp_unconstrained_lorenz.gif"), fps = 1) # hide
+prior = combine_distributions(priors)
 
+N_ens = 20
+N_iter = 20
+N_trials = 50
+errs = zeros(N_trials,N_iter)
+errs_acc = zeros(N_trials,N_iter)
+errs_acc_cs = zeros(N_trials,N_iter)
 
-# plots in constrained space
-ϕ_init = transform_unconstrained_to_constrained(priors, u_init)
-anim_ekp_lorenz = @animate for i in 1:N_iter
-    ϕ_i = get_ϕ(priors, ekiobj, i)
-    p = plot(ϕ_i[1, :], ϕ_i[2, :], seriestype = :scatter, xlims = extrema(ϕ_init[1, :]), ylims = extrema(ϕ_init[2, :]))
-    vline!(
-        [params_true[1]],
-        xaxis = "ϕ1",
-        yaxis = "ϕ2",
-        linestyle = :dash,
-        linecolor = :red,
-        label = false,
-        title = "EKI iteration = " * string(i),
-    )
-    hline!([params_true[2]], linestyle = :dash, linecolor = :red, label = "optimum")
+for trial in 1:N_trials
+    initial_ensemble = EKP.construct_initial_ensemble(rng, prior, N_ens)
+
+    # We create 3 EKP Inversion objects to compare acceleration.
+    ekiobj_vanilla = EKP.EnsembleKalmanProcess(initial_ensemble, y, Γ, Inversion(); rng = rng)
+    ekiobj_acc = EKP.EnsembleKalmanProcess(initial_ensemble, y, Γ, Inversion(); rng = rng, accelerator=NesterovAccelerator())
+    ekiobj_acc_cs = EKP.EnsembleKalmanProcess(initial_ensemble, y, Γ, Inversion(); rng = rng, accelerator=ConstantStepNesterovAccelerator())
+
+    err = zeros(N_iter)
+    err_acc = zeros(N_iter)
+    err_acc_cs = zeros(N)
+    for i in 1:N_iter
+        g_ens_vanilla = G(get_ϕ_final(prior, ekiobj_vanilla))
+        EKP.update_ensemble!(ekiobj_vanilla, g_ens_vanilla, deterministic_forward_map = true)
+        g_ens_acc = G(get_ϕ_final(prior, ekiobj_acc))
+        EKP.update_ensemble!(ekiobj_acc, g_ens_acc, deterministic_forward_map = true)
+        g_ens_acc_cs = G(get_ϕ_final(prior, ekiobj_acc_cs))
+        EKP.update_ensemble!(ekiobj_acc_cs, g_ens_acc_cs, deterministic_forward_map = true)
+    end
+    errs[trial,:] = get_error(ekiobj_vanilla)
+    errs_acc[trial,:] = get_error(ekiobj_acc)
+    errs_acc_cs[trial,:] = get_error(ekiobj_acc_cs)
 end
-gif(anim_ekp_lorenz, joinpath(figure_save_directory, "ekp_lorenz.gif"), fps = 1) # hide
+
+# COMPARE CONVERGENCES
+
+convplot = plot(1:(N_iter), mean(errs, dims=1)[:], color = :black, label = "No acceleration")
+plot!(1:(N_iter), mean(errs_acc, dims=1)[:], color = :blue, label = "Nesterov Accelerator")
+plot!(1:(N_iter), mean(errs_acc_cs, dims=1)[:], color = :red, label = "Nesterov Accelerator, Constant Step")
+title!("EKI convergence on Lorenz96 IP, N_trials="*string(N_trials))
+
+# error bars
+plot!(1:(N_iter), (mean(errs, dims=1)[:] + std(errs, dims=1)[:]/sqrt(N_trials)), color = :black, ls= :dash, label = "")
+plot!(1:(N_iter), (mean(errs, dims=1)[:] - std(errs, dims=1)[:]/sqrt(N_trials)), color = :black, ls= :dash, label = "")
+plot!(1:(N_iter), (mean(errs_acc, dims=1)[:] + std(errs_acc, dims=1)[:]/sqrt(N_trials)), color = :blue, ls= :dash, label = "")
+plot!(1:(N_iter), (mean(errs_acc, dims=1)[:] - std(errs_acc, dims=1)[:]/sqrt(N_trials)), color = :blue, ls= :dash, label = "")
+plot!(1:(N_iter), (mean(errs_acc_cs, dims=1)[:] + std(errs_acc_cs, dims=1)[:]/sqrt(N_trials)), color = :red, ls= :dash, label = "")
+plot!(1:(N_iter), (mean(errs_acc_cs, dims=1)[:] - std(errs_acc_cs, dims=1)[:]/sqrt(N_trials)), color = :red, ls= :dash, label = "")
+
+display(convplot)
+
