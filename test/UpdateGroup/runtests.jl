b0952524ba79f95e67e9394575bb3f0540198102
odunbar <odunbar@caltech.edu>
2024-11-13 16:01:55 -0800

    bugfix in tests

--- test/UpdateGroup/runtests.jl
+++ test/UpdateGroup/runtests.jl
@@ -22,7 +22,7 @@ const EKP = EnsembleKalmanProcesses
     group1 = UpdateGroup(u1, g1, Dict("1:8" => "1:20"))
     @test get_u_group(group1) == u1
     @test get_g_group(group1) == g1
-    @test get_group_id(group) == Dict("1:8" => "1:20")
+    @test get_group_id(group1) == Dict("1:8" => "1:20")
 
     u1c = [2, 4, 9, 10]
     g2 = 3:9
@@ -34,10 +34,8 @@ const EKP = EnsembleKalmanProcesses
 
     # break consistency with different ways
     not_u1c = [2, 3, 4, 9, 10]
-    group_bad = UpdateGroup([], g2)
-    @test_throws ArgumentError update_group_consistency([group1, group_bad], input_dim, output_dim)
-    group_bad = UpdateGroup(u1c, [])
-    @test_throws ArgumentError update_group_consistency([group1, group_bad], input_dim, output_dim)
+    @test_throws ArgumentError UpdateGroup([], g2)
+    @test_throws ArgumentError group_bad = UpdateGroup(u1c, [])
     group_bad = UpdateGroup(not_u1c, g2) # [u1,not_u1c] is not a partition of 1:10
     @test_throws ArgumentError update_group_consistency([group1, group_bad], input_dim, output_dim)
     not_inbd_g = [21] # outside of 1:20
