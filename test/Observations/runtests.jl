6c979c0cd95e4482cf38b1e294cdb5f63bc04d65
odunbar <odunbar@caltech.edu>
2025-03-19 13:20:58 -0700

    matrix vec tests

--- test/Observations/runtests.jl
+++ test/Observations/runtests.jl
@@ -444,11 +444,24 @@ end
     @test norm(test1 - out1) < 1e-12
     @test norm(test2 - out2) < 1e-12
 
+    #sqrt_cov
+    test1 = sqrt(Γ[g_idx, g_idx]) * Xvec[g_idx]
+    test2 = sqrt(Γ[g_idx, g_idx]) * X[g_idx, :]
+    out1 = zeros(size(test1))
+    out2 = zeros(size(test2))
+    lmul_sqrt_obs_noise_cov!(out1, observation_series, Xvec[g_idx], g_idx)
+    lmul_sqrt_obs_noise_cov!(out2, observation_series, X[g_idx, :], g_idx)
+    @test norm(test1 - out1) < 1e-12
+    @test norm(test2 - out2) < 1e-12
+    lmul_sqrt_obs_noise_cov!(out1, observation_series, Xvec, g_idx)
+    lmul_sqrt_obs_noise_cov!(out2, observation_series, X, g_idx)
+    @test norm(test1 - out1) < 1e-12
+    @test norm(test2 - out2) < 1e-12
+    
+    
     # cov_inv
     test1 = Γinv[g_idx, g_idx] * Xvec[g_idx]
     test2 = Γinv[g_idx, g_idx] * X[g_idx, :]
-    out1 = zeros(size(test1))
-    out2 = zeros(size(test2))
     out1 = zeros(size(test1))
     out2 = zeros(size(test2))
     # In code this is applied to pre-trimmed "X"s or not
@@ -458,6 +471,21 @@ end
     @test norm(test2 - out2) < 1e-12
     lmul_obs_noise_cov_inv!(out1, observation_series, Xvec, g_idx)
     lmul_obs_noise_cov_inv!(out2, observation_series, X, g_idx)
+    @test norm(test1 - out1) < 1e-12
+    @test norm(test2 - out2) < 1e-12
+    
+    # sqrt_cov_inv
+    test1 = sqrt(Γinv[g_idx, g_idx]) * Xvec[g_idx]
+    test2 = sqrt(Γinv[g_idx, g_idx]) * X[g_idx, :]
+    out1 = zeros(size(test1))
+    out2 = zeros(size(test2))
+    # In code this is applied to pre-trimmed "X"s or not
+    lmul_sqrt_obs_noise_cov_inv!(out1, observation_series, Xvec[g_idx], g_idx)
+    lmul_sqrt_obs_noise_cov_inv!(out2, observation_series, X[g_idx, :], g_idx)
+    @test norm(test1 - out1) < 1e-12
+    @test norm(test2 - out2) < 1e-12
+    lmul_sqrt_obs_noise_cov_inv!(out1, observation_series, Xvec, g_idx)
+    lmul_sqrt_obs_noise_cov_inv!(out2, observation_series, X, g_idx)
     @test norm(test1 - out1) < 1e-12
     @test norm(test2 - out2) < 1e-12
 
